82fa2e50d4c6669be09c5f1e8c7c5c33730bf9c4
NestorAec26 <143562624+NestorAec26@users.noreply.github.com>
2023-09-29 14:53:38 -0500

    Add files via upload

---
+++ EVA1_10_INICIALIZACION/src/eva1_10_inicializacion/EVA1_10_INICIALIZACION.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_10_inicializacion;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_10_INICIALIZACION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //INICIALIZACION DE VARIABLES
+        int valor  = 0;
+        System.out.println(valor);
+        int precio;// DECLARAR
+        precio = 100; // INICIALIZAR//
+        
+        double salario = 0 , precioMayoreo = 100,;
+        
+        final int conteo;
+        conteo = 100;
+        System.out.println(conteo);
+        
+        final double MI_VALOR_PI = 3.1416;
+        final String ESCUELA = "Instituto tecnologico ll de chihuahua";
+        final int CALIFICAR_PASAR = 70;
+        
+        int X;
+        
+        // TODO code application logic here
+    }
+    
+}
